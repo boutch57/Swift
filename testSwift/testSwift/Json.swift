fcb1fa560e24d52c22d5c26c64c3818be741b698
boutch57 <32631469+boutch57@users.noreply.github.com>
2017-12-22 20:45:48 +0100

    Add files via upload

---
+++ testSwift/testSwift/Json.swift
@@ -0,0 +1,19 @@
+//
+//  Json.swift
+//  testSwift
+//
+//  Created by Jordan Bouaoun on 22/12/2017.
+//  Copyright © 2017 Jordan Bouaoun. All rights reserved.
+//
+
+import Foundation
+
+
+class Json {
+    
+    // API Request
+    //let json = JSON.fromURL('https://jsonplaceholder.typicode.com/posts");
+    
+    
+    
+}
---
+++ testSwift/testSwift/TableController.swift
@@ -0,0 +1,9 @@
+//
+//  TableController.swift
+//  testSwift
+//
+//  Created by Jordan Bouaoun on 22/12/2017.
+//  Copyright © 2017 Jordan Bouaoun. All rights reserved.
+//
+
+
